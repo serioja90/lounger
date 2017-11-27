2c457624d6041b2d37ee7fe0eaf6689f89025aed
Groza Sergiu <serioja90@gmail.com>
2017-11-27 20:32:35 +0100

    NEW: track pending signals, so that they could be considered on the next wait call

--- lib/lounger.rb
+++ lib/lounger.rb
@@ -4,20 +4,33 @@ class Lounger
   SIGNALS = ["INT", "TERM", "EXIT", "USR1", "QUIT"]
 
   def initialize(include_signals: [], exclude_signals: [])
-    @lock      = Mutex.new
-    @condition = ConditionVariable.new
+    @lock            = Mutex.new
+    @condition       = ConditionVariable.new
+    @pending_signals = 0
 
     (SIGNALS + include_signals - exclude_signals).each do |signal|
       Signal.trap(signal) { wakeup! }
     end
   end
 
-  def idle
-    @lock.synchronize { @condition.wait(@lock) }
+  def idle(ignore_pending: false)
+    @lock.synchronize do
+      @pending_signals = 0 if ignore_pending
+
+      if @pending_signals > 0
+        @pending_signals -= 1
+      else
+        @condition.wait(@lock)
+        @pending_signals -= 1
+      end
+    end
   end
 
   def wakeup!
-    @condition.signal
+    @lock.synchronize do
+      @pending_signals += 1
+      @condition.signal
+    end
   end
 
   def self.idle
